ddf7b83c37a07440811ea3385d0662e4ddc2c890
muneebmanzoor230 <muneebmanzoor230@gmail.com>
2026-04-09 15:31:52 +0530

    Modify home route to include new message
    Update home route response message.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello Muneeb 🚀 Your CI/CD is LIVE!"
-
+    return "🚀 Muneeb's CI/CD Project is LIVE"
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
